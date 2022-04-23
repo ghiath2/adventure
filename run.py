e61960451b9f2b47c27e63ff7a7037e36446e754
ghiath2 <ghiathht3@gmail.com>
2022-04-23 15:47:34 +0000

    Add entery function and print the contexts

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-
 import sys
 import os
 import random
 import time
+
+
+class Game:
 
     def print_slow(self, str, delay=0.1):
         for letter in str:
@@ -25,3 +27,26 @@ import time
     def sprint(self, str, delay=0):
         print(str)
         time.sleep(delay)
+
+
+class World:
+
+    def entry(self):
+        hero.location = "entry"
+        print(f"\nHealth: {hero.health}")
+        game_functions.fprint("You are in a dark cave. The entry has been sealed by fallen rocks. There is no way out.", 2)
+        print("Ahead, you can see a cavern. Will you continue?")
+        print("Enter 'yes' or 'no'.")
+        self.check_medkit()
+        self.handle_goblin()
+        while True:
+            action = input("\n> ")
+            if action == "yes":
+                self.cavern()
+            elif action == "no":
+                game_functions.fprint(
+                    "A bat flies over your head and you hear screetches in the distance.")
+            elif action == "m":
+                self.use_medkit()
+            else:
+                game_functions.fprint("You sit in total darkness wondering if there's a way out.")
